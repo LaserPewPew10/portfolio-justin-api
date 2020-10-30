498a8286eaf02745a590656cd2d99254a1223110
Justin Ramirez <ramirezbjustin@gmail.com>
2020-10-30 19:01:58 -0400

    updated portfolio on server side

--- routes/portfolios.js
+++ routes/portfolios.js
@@ -6,11 +6,13 @@ const {
   getPortfolios,
   getPortfolioById,
   createPortfolio,
+  updatePortfolio,
 } = require("../controllers/portfolios");
 
 router.get("", getPortfolios);
 router.get("/:id", getPortfolioById);
-
+// TODO: create middleware to check for admin rights!!
 router.post("", checkJwt, createPortfolio);
+router.patch("/:id", checkJwt, updatePortfolio);
 
 module.exports = router;
