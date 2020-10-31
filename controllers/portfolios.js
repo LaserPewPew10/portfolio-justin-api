a3a2dbf849429bd18183203e06a415ed9e3832a4
Justin Ramirez <ramirezbjustin@gmail.com>
2020-10-30 20:13:50 -0400

    trying to get delete functionality

--- controllers/portfolios.js
+++ controllers/portfolios.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Portfolio = mongoose.model("Portfolio");
 
 exports.getPortfolios = async (req, res) => {
-  const portolfios = await Portfolio.find({});
-  return res.json(portolfios);
+  const portfolios = await Portfolio.find({});
+  return res.json(portfolios);
 };
 
 exports.getPortfolioById = async (req, res) => {
@@ -37,13 +37,15 @@ exports.updatePortfolio = async (req, res) => {
     const updatedPortfolio = await Portfolio.findOneAndUpdate(
       { _id: id },
       body,
-      {
-        new: true,
-        runValidators: true,
-      }
+      { new: true, runValidators: true }
     );
     return res.json(updatedPortfolio);
   } catch (error) {
     return res.status(422).send(error.message);
   }
+};
+
+exports.deletePortfolio = async (req, res) => {
+  const portfolio = await Portfolio.findOneAndRemove({ _id: req.params.id });
+  return res.json({ _id: portfolio.id });
 };
--- routes/portfolios.js
+++ routes/portfolios.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const { checkJwt } = require("../controllers/auth");
-
 const {
   getPortfolios,
   getPortfolioById,
   createPortfolio,
   updatePortfolio,
+  deletePortfolio,
 } = require("../controllers/portfolios");
 
 router.get("", getPortfolios);
 router.get("/:id", getPortfolioById);
-// TODO: create middleware to check for admin rights!!
+
+// TODO: create middleware to check for admin rights!!!!
 router.post("", checkJwt, createPortfolio);
 router.patch("/:id", checkJwt, updatePortfolio);
+router.delete("/:id", checkJwt, deletePortfolio);
 
 module.exports = router;
