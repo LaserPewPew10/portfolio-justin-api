c581ddac37617c6a6263cf6c6aa71a558b35194a
Justin Ramirez <ramirezbjustin@gmail.com>
2020-11-25 17:51:32 -0500

    adding ing production file for heroku deployment

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    DB_URI: process.env.DB_URI,
+    AUTH0_NAMESPACE: process.env.DB_URI,
+    AUTH0_TOKEN_URL: process.env.DB_URI,
+    AUTH0_CLIENT_ID: process.env.DB_URI,
+    AUTH0_CLIENT_SECRET: process.env.DB_URI,
+    AUTH0_AUDIENCE: process.env.DB_URI,
+    AUTH0_DOMAIN: process.env.DB_URI,
+  };
+
