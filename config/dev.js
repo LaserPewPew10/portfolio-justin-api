ad242573d07d8f0e7192b0910f8f05f4d1ac3c6a
Justin Ramirez <ramirezbjustin@gmail.com>
2020-12-09 16:07:03 -0500

    placing gitignore for api

--- config/dev.js
+++ config/dev.js
@@ -1,10 +1,11 @@
 module.exports = {
-  DB_URI:
-    "mongodb+srv://justin_ramirezzz:messi%23071995@cluster0.wqlsj.mongodb.net/cluster0?retryWrites=true&w=majority",
-  AUTH0_NAMESPACE: "https://portfolio-justin.com",
-  AUTH0_TOKEN_URL: 'https://justinramirez.us.auth0.com/oauth/token',
-  AUTH0_CLIENT_ID: 'F1riYz7xVWUW4I464xOaiKFXBApRLc03',
-  AUTH0_CLIENT_SECRET: 'tJOvDWSmy8_ezRl1oQgvtoj6R1nvPzHelSB5OUnXGa0lJryQGT-SkNrbrtDCjz5O',
-  AUTH0_AUDIENCE:'https://justinramirez.us.auth0.com/api/v2/',
-  AUTH0_DOMAIN: 'https://justinramirez.us.auth0.com'
-};
+    DB_URI:
+      "mongodb+srv://justin_ramirezzz:messi%23071995@cluster0.wqlsj.mongodb.net/cluster0?retryWrites=true&w=majority",
+    AUTH0_NAMESPACE: "https://portfolio-justin.com",
+    AUTH0_TOKEN_URL: 'https://justinramirez.us.auth0.com/oauth/token',
+    AUTH0_CLIENT_ID: 'F1riYz7xVWUW4I464xOaiKFXBApRLc03',
+    AUTH0_CLIENT_SECRET: 'tJOvDWSmy8_ezRl1oQgvtoj6R1nvPzHelSB5OUnXGa0lJryQGT-SkNrbrtDCjz5O',
+    AUTH0_AUDIENCE:'https://justinramirez.us.auth0.com/api/v2/',
+    AUTH0_DOMAIN: 'https://justinramirez.us.auth0.com'
+  };
+  
