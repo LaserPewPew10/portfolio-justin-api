d573fe6a8fa0ce1e856d77bff803ca1868af73c2
Justin Ramirez <ramirezbjustin@gmail.com>
2020-12-22 16:02:19 -0500

    adding dev.js

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+    DB_URI:
+      "mongodb+srv://justin_ramirezzz:messi%23071995@cluster0.wqlsj.mongodb.net/cluster0?retryWrites=true&w=majority",
+    AUTH0_NAMESPACE: "https://portfolio-justin.com",
+    AUTH0_TOKEN_URL: 'https://justinramirez.us.auth0.com/oauth/token',
+    AUTH0_CLIENT_ID: '24qC514avlkoMo7MJ2pLDPqjSZd2SzMB',
+    AUTH0_CLIENT_SECRET: 'BG123aQaZS1_xcZk4GACh02qjzxsLQAsLhpPX767_POxDvHw3zJF_r1fMcVj82w7',
+    AUTH0_AUDIENCE:'https://justinramirez.us.auth0.com/api/v2/',
+    AUTH0_DOMAIN: 'https://justinramirez.us.auth0.com'
+  };
